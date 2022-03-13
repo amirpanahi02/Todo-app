f01a0c65068ea9af7a52e9eba14daf65abc3509b
Amirhossein Panahi <76567918+amirpanahi02@users.noreply.github.com>
2022-03-13 12:49:10 +0330

    add throttle

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,7 +1,7 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
+import { createStore } from "redux";
 import { loadState, saveState } from "./localStorage";
 import reducers from "./reducers";
+import throttle from "lodash.throttle";
 
 const devtools = // @ts-ignore
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
@@ -11,7 +11,9 @@ const persistedState = loadState();
 const store = createStore(reducers, persistedState, devtools);
 export type RootState = ReturnType<typeof store.getState>;
 
-store.subscribe(() => {
-  saveState(store.getState());
-});
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
+);
 export default store;
